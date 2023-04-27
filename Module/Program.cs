9e92d87d964f7b08f0c5fe94e060c35f7e9e6927
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-04-27 11:20:01 +0300

    5.2.8

--- Module/Program.cs
+++ Module/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Modul
+{
+    internal class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массиваномер {0}", i + 1 );
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            return result;
+        }
+        static int[] SortArray(int[] result)
+        {
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+
+            return result;
+        }
+    }
+}
